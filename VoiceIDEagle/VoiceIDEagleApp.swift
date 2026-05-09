a1911437fbc750d57855a45823d7a525bd338b2f
Amit Mahadik <amit.mahadik@gmail.com>
2026-05-09 13:00:36 -0700

    Initial Commit

---
+++ VoiceIDEagle/VoiceIDEagleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceIDEagleApp.swift
+//  VoiceIDEagle
+//
+//  Created by Mahadik, Amit on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceIDEagleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
